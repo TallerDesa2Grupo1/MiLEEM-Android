f39f275df04ffd55e4a3fdaf90fd0a8b3208d10f
romicorty <romicorty@romicorty-Inspiron-1545.(none)>
2014-09-21 00:53:48 -0300

    Proyecto inicial: S2-4/S2-5

---
+++ MiLEEM/src/ar/uba/fi/mileem/MainActivity.java
@@ -0,0 +1,29 @@
+package ar.uba.fi.mileem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import ar.uba.fi.mileem.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button btnGo =  (Button) findViewById(R.id.buttonGo);
+		btnGo.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this,SimpleFormActivity.class);
+				MainActivity.this.startActivity(i);
+			}
+		});
+	}
+
+
+
+
+}
